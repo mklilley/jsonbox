de5f281fcad45fda7850cd5c250cbbe6d0d06b39
Matt Lilley <m.k.lilley@gmail.com>
2020-11-29 12:18:06 +0000

    Increased the rate limit for a more realistic use rate

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	SIZE_LIMIT: 50, // mentioned in KB
 	PORT: process.env.PORT || 3000,
 	MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
-	REQUEST_LIMIT_PER_HOUR: 100,
+	REQUEST_LIMIT_PER_HOUR: 1000,
 	ENABLE_DATA_EXPIRY: true, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
 	DATA_EXPIRY_IN_DAYS: 365,
 	FILTER_IP_SET: [],  // example ['172.29.0.1']
